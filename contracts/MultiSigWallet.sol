98559b09073b9a9006eaa323d4fe8fac091e2324
ayush-3516 <letsmail.ayushchaudhari@gmail.com>
2022-07-08 22:34:42 +0530

    Create MultiSigWallet.sol

---
+++ contracts/MultiSigWallet.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract MultiSigWallet {
+    event Deposit(address indexed sender, uint256 amount);
+    event Submit(uint256 indexed txId);
+    event Approve(address indexed owner, uint256 indexed txId);
+    event Revoke(address indexed owner, uint256 indexed txId);
+    event Execute(uint256 indexed txId);
+}
